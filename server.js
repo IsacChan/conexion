34bc9dedc757626db6ad1702c4271493409e55a6
Isac Chan <isacchanchim@gmail.com>
2026-02-26 10:09:12 -0600

    act

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  multipleStatements: true
 });
 
 db.connect(err => {
